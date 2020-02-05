4796ec2bbc52f9b94344e05f3876042e8418549b
Guilherme Alberto de Moraes <guilherme.moraes@camss.group>
2020-02-05 17:22:36 -0300

    Correção de validação do cliente

--- src/main/java/com/example/banking/Controller.java
+++ src/main/java/com/example/banking/Controller.java
@@ -7,21 +7,37 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	
+
 	@Autowired
 	ClientRepository clientRepository;
-	
+
 	@PostMapping("/clients/card/")
-	public Client activateCard(@RequestBody String cardNumber) {
+	public Client card(@RequestBody String action, @RequestBody String cardNumber, @RequestBody String activationCode) {
+
+		if (action.equals("activateCard")) {
+			Client client = findClient(cardNumber);
+			validateActivationCode(client, activationCode);
+			client.setActivated(true);
+			return clientRepository.save(client);
+		}
+
+		throw new RuntimeException();
+	}
+
+	private void validateActivationCode(Client client, String activationCode) {
+		if (!client.getActivationCode().equals(activationCode)) {
+			throw new ForbiddenException();
+		}
+	}
+
+	private Client findClient(String cardNumber) {
 		Client retrivedClient = clientRepository.findByCardNumber(cardNumber);
 
-		if(retrivedClient == null){
+		if (retrivedClient == null) {
 			throw new NotFoundException();
 		}
 
-		retrivedClient.setActivated(true);
-		Client savedClient = clientRepository.save(retrivedClient);
-		
-		return savedClient;
+		return retrivedClient;
+
 	}
 }
---
+++ src/main/java/com/example/banking/ForbiddenException.java
@@ -0,0 +1,18 @@
+package com.example.banking;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * ForbiddenException
+ */
+@@ResponseStatus(code = HttpStatus.FORBIDDEN)
+public class ForbiddenException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    
+}
