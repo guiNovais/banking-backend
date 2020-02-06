895bbda64834505afaafd69c86d6e5cfd6bb26a1
Guilherme Alberto de Moraes <guilherme.moraes@camss.group>
2020-02-06 16:08:36 -0300

    Correção do caminho do controller

--- src/main/java/com/example/banking/Controller.java
+++ src/main/java/com/example/banking/Controller.java
@@ -5,14 +5,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.java.Log;
+
+@Log
 @RestController
 public class Controller {
 
 	@Autowired
 	ClientRepository clientRepository;
 
-	@PostMapping("/clients/card/")
+	@PostMapping("/card")
 	public void operation(@RequestBody OperationDTO body) {
+		log.info(body.toString());
 
 		switch (body.getAction()) {
 		case ACTIVATE:
@@ -35,6 +39,9 @@ public class Controller {
 	}
 
 	private void activateCard(String cardNumber, String activationCode) {
+		log.info("activateCard(String, String) invoked");
+		log.info(cardNumber);
+		log.info(activationCode);
 		Client client = findClient(cardNumber);
 		validateActivationCode(client, activationCode);
 		client.setActivated(true);
@@ -42,15 +49,21 @@ public class Controller {
 	}
 
 	private void validateActivationCode(Client client, String activationCode) {
+		log.info("validateActivationCode(Client, String), invoked");
+		log.info(client.toString());
+		log.info(activationCode);
 		if (!client.getActivationCode().equals(activationCode)) {
+			log.info(String.format("activationCode %s invalid!", activationCode));
 			throw new ForbiddenException();
 		}
 	}
 
 	private Client findClient(String cardNumber) {
+		log.info("findClient(String) invoked");
 		Client retrivedClient = clientRepository.findByCardNumber(cardNumber);
 
 		if (retrivedClient == null) {
+			log.info(String.format("Client with cardNumber %s not found", cardNumber));
 			throw new NotFoundException();
 		}
 
